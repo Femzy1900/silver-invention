8579513eadf859c160fe7f26beae8c9906c7394b
unknown <adedokunfemi14@gmail.com>
2024-01-14 06:29:13 -0800

    Creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Sidebar from "./components/sidebar/Sidebar"
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+    <Sidebar />
   )
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="w-[104px] h-[834px] bg-[#1F1D2B]">
+        Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
