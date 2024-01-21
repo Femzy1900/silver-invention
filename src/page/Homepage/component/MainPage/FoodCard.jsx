3e90130888654688e51d3f3065a2330857b020e6
Femzy1900 <adedokunfemi14@gmail.com>
2024-01-21 14:42:13 -0800

    Going well

--- src/page/Homepage/component/MainPage/FoodCard.jsx
+++ src/page/Homepage/component/MainPage/FoodCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { items } from '../../../../data/db'
+import {FaChevronDown} from "react-icons/fa"
 
 
 const FoodCard = () => {
   
   return (
     <div className="">
-        <div className="">
+        <div className="flex items-center">
             <p>Choose Dishes</p>
-            <div className="">
-
+            <div className="flex items-center rounded-lg p-2 bg-[#1F1D2B]">
+              <FaChevronDown />
+              <p>Dine</p>
             </div>
         </div>
         {items.map((item) => (
