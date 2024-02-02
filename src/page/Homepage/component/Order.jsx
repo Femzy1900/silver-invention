2d5d15aeea4416cc2f0a6d3ad5988c06b7b82ef6
Femzy1900 <adedokunfemi14@gmail.com>
2024-02-02 15:23:52 -0800

    order side

--- src/page/Homepage/component/Order.jsx
+++ src/page/Homepage/component/Order.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const Order = () => {
   return (
     <div className='flex p-[24px] text-white flex-col gap-2 w-[410px] bg-[#1F1D2B] '>
-      <div className="bg-white">
-
-        <h1>Hello</h1>
+      <h1 className='font-bold text-lg'>Orders #34562</h1>
+      <div className="flex gap-4">
+        <p className='rounded-md bg-[#EA7C69] p-2 text-sm'>Dine in</p>
+        <p className='rounded-md border-black p-2 text-sm'>To Go</p>
+        <p>Delivery</p>
       </div>
-      
     </div>
   )
 }
