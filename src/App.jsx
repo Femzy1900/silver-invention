906d3671c7e27b69918cef4c7dfe7e3d7bf98942
Femzy1900 <adedokunfemi14@gmail.com>
2024-02-04 15:17:23 -0800

    Another dat fuck nepa

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 export default function App() {
   return (
-    <div className="flex">
-
-      <Sidebar />
-      <Homepage />
-    </div>
+    <Router >
+        <div>
+            <Sidebar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            
+          </Routes>
+        </div>
+    </Router>
+   
     )
 }
